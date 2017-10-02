ecc85bbc52e66051ea74664f2c0e75b0befd55e2
lxuds <leixuast@gmail.com>
2017-10-01 22:53:35 -0700

    clean

--- Code/gen_feat_header.py
+++ Code/gen_feat_header.py
@@ -7,38 +7,18 @@ from sklearn.preprocessing import MultiLabelBinarizer
 df = pd.read_csv('./data/Interview_Mapping.csv')
 
 #df = df.loc[df['Area.of.Law'] =='To be Tested']
-'''
-from collections import Counter
-c = Counter()
-l = np.array([1, 2, 1, 4, 1])
-for i in range(len(l)):
-   c[i]=l[i]
 
 
-t = np.array(list(c.elements()))
-
-ll = np.random.choice(l, size=l.shape, replace=True)
-'''
 # extract category indicator
 names =[]
 for i in range(df.shape[0]):
-#for j in range(0,48):
-#    i = l[j]
     f = open('./data/FixedJudgements/'+ df.iloc[i,0] + '.txt')
     raw = f.read()
     text1 = raw.split('ADVOCATES')[0]
     text2 = text1.split('Judges')[1]
-    #print text2
-    #pattern = r'^([a-z\. ]+) [0-9]'
-    #p1 = r'^([a-z\. ]+) (?:[0-9])'
     pattern = r'^([a-z][a-z-\. ]+)(?:[0-9]|Case|Appeal|\()'
     obj = re.findall(pattern, text2, re.I|re.MULTILINE)
-    #print i, df.iloc[i,0],  obj
-    #tmp = "ADVOCATES" in raw
-    #tmp = "Judges" in raw
-    #print tmp
     names.append(obj)
-#print names
 
 
 clean_name = []
@@ -53,10 +33,6 @@ for i, x in enumerate(names):
        a = a.replace('DOJ', '')
        a = a.replace('CIV. RULE', 'CIVIL')
        a = a.replace('I.-T. REF', 'INCOME TAX')
-       #a = re.sub(r'C. R.', 'C.R.', a )
-       #a = re.sub(r'C.R\z', 'C.R.', a )
-       #a = a.replace('C. R.', 'C.R.')
-       #a = a.replace('CR', 'C.R.')
        a = a.replace('A. O. D.', 'A.O.D.')
        a = a.replace('S. A. W O.', 'S.A.W.O.')
        a = a.rstrip()
@@ -94,20 +70,16 @@ for i, x in enumerate(names):
 df['source'] = clean_name
 b = df['source'].value_counts()
 b.to_csv('a.csv', index=True)
-#idn =[i for i in range(len(clean_name)) if clean_name[i] =='No.DOJ']
-#print idn
-#print df["Judgements"].iloc[idn]
-print b
 
 threshold = 2
 value_counts = df['source'].value_counts() # Specific column 
-print value_counts
-print dir(value_counts)
+#print value_counts
+#print dir(value_counts)
 to_remove = value_counts[value_counts < threshold].index
 df['source'].replace(to_remove, 'OTHER', inplace=True)
 df['source'].replace('', 'MISSING', inplace=True)
 b = df['source'].value_counts()
-print b
+#print b
 
 
 #print df.groupby(['Area.of.Law','source']).size() #.order(ascending=False)
@@ -130,47 +102,10 @@ df_train = df.loc[df['Area.of.Law'] !='To be Tested']
 
 Y_train = df_train['Area.of.Law']
 
-#print pd.crosstab(df_train['Area.of.Law'], df_train['source'])
-groupby_source = df_train['source'].groupby(df_train['Area.of.Law'])
-print groupby_source.describe()
-
-print df_train.info()
-#print all(names)
-#print l
-
-
-
-
-   
-    #HON?BLE
-    #print text1
-    pattern = r'(?:BLE|CHIEF) (?:JUSTICE M(?:R|S|RS).|M(?:R|S|RS). JUSTICE) ([a-z\. ]+)(?:,|&|\n|A.F.O.D.)'
-
-    obj = re.findall(pattern, text1, re.I)
-    print i, df_train.iloc[i,0],  obj
-    obj = [x.rstrip() for x in obj]
-    #pattern1 = r'([A-Z]\.)*([A-Z]*)'
-    #pattern_last_name = r'[ \.]([A-Z]*)$'
-    #obj = re.findall(pattern_last_name, obj[1], re.I)
-    pattern1 = r'([A-Z]\.)([A-Z]\.)([A-Z]*)'
-    obj = re.match(pattern1, obj[0], re.I)
-    print obj.group()
-    print obj.group(1)+obj.group(2) + ' ' + obj.group(3)
-    print ''.join([obj.group(1), obj.group(2), ' ', obj.group(3)])
-    names.append(obj)
-    #pattern = r'(?:BLE|CHIEF) (?:JUSTICE M[R|S|RS].|M[R|S|RS]. JUSTICE) ([a-z\. ]+)[,|&|\n]'
-
-#df_train['judges']= names
-#df_train.info()
-#print df_train.head()
-
-#([A-Z]\.)+([A-Z]*)
-
-
 
 d = {}
 #df_train = df.loc[df['Area.of.Law'] !='To be Tested']
-
+'''
 
 # extract names
 names = []
@@ -269,112 +204,13 @@ for i in range(df.shape[0]):
     else:
         court_loc = ''
     court_loc_list.append(court_loc)
-print court_loc_all
-print court_loc_list
+#print court_loc_all
+#print court_loc_list
 df['court_loc'] = court_loc_list
 
 df_train = df.loc[df['Area.of.Law'] !='To be Tested']
-print df_train['Area.of.Law'].groupby(df_train['court_loc']).describe()
+#print df_train['Area.of.Law'].groupby(df_train['court_loc']).describe()
 
-print df_train.groupby(['court_loc', 'Area.of.Law']).size()
+#print df_train.groupby(['court_loc', 'Area.of.Law']).size()
 
-for i in range(df_train.shape[0]):
-#for i in range(868, 869):
-    f = open('./data/FixedJudgements/'+ df_train.iloc[i,0] + '.txt')
-    raw = f.read()
-    text1 =  raw.split('Judgment')[0]
-    pattern = r'(?:BLE|CHIEF) (?:JUSTICE M(?:R|S|RS).|M(?:R|S|RS). JUSTICE) ([a-z\. ]+)(?:,|&|\n|A.F.O.D.)'
-
-    obj = re.findall(pattern, text1, re.I)
-    obj = [x.rstrip() for x in obj] 
-    names.append(obj)
-    name_list_new = []
-    for name in obj:
-        if '.' in name:
-            pattern =r'([A-Z]\.[A-Z]\.)([A-Z]{2,})'
-            obj_new = re.findall(pattern, name, re.I)
-            if obj_new:
-                new_name = obj_new[0][0] + ' ' + obj_new[0][1]
-            else:
-                new_name = name
-        elif '.' not in name: 
-            a_list = name.split(' ') 
-            if len(a_list) == 1:
-                new_name = name
-            else:
-                first_l = [a_list[j][0] for j in range(0,len(a_list)-1) ]
-                new_name = '.'.join(first_l+ [' ']) + a_list[-1]
-          
-        name_list_new.append(new_name)
-    print i, df_train.iloc[i,0], obj, name_list_new
-
-names_judgment =[]
-#for i in range(df_train.shape[0]):
-for i in range(331, 332):
-    f = open('./data/FixedJudgements/'+ df_train.iloc[i,0] + '.txt')
-    raw = f.read()
-    text1 =  raw.split('Judgment')[1]
-    text2 = text1.splitlines()
-    #text2 = text1.split('\n')[0]
-    #HON?BLE
-    #print text1
-    print 'text2[0]', text2[0]
-    print 'text2[1]', text2[1]
-    #print text2[2:10]
-    pattern = r'^([a-z\. ]+)[, .]+J.'
-    if text2[0]: 
-        judge_text = text2[0]
-    else:
-       judge_text = text2[1]
-    obj = re.findall(pattern, judge_text, re.I)
-    print i, df_train.iloc[i,0],  obj, judge_text
-    #names.append(obj)
-
-
-#print df_train.index[df_train.isnull().any(axis=1)]
-
-#print [i for i, x in enumerate(names) if not x ]
-
-
-f = open('./data/FixedJudgements/'+ df_train.iloc[0,0] + '.txt')
-raw = f.read()
-pattern = r'THE HONOURABLE [CHIEF] (?:JUSTICE MR.|MR. JUSTICE) ([a-z\. ]+)[,|&|\n]'
-obj = re.findall(pattern, raw, re.I)
-print obj
-
-
-
-def drop_html(html):
-    return BeautifulSoup(html,"lxml").get_text(separator=" ",strip=True)
-
-
-raw = drop_html(raw)
-
-
-my_list = raw.splitlines()
-
-
-pattern = r'JUSTICE MR.|MR. JUSTICE ([a-z.\s]+)'
-re.findall(pattern, raw)
-
-
-fout = 'vocab.txt'
-fo = open(fout, "w")
-for k, v in vec_tfidf.vocabulary_.items():
-    fo.write(str(k) + ' >>> '+ str(v) + '\n\n')
-
-
-
-
-fo.close()
-
-x_train2[0]
-
-
-
-plt.hist(Y_train)
-plt.show()
-plt.savefig("foo.png")
-plt.close()
-'''
 
